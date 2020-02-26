65a3e840af0a9bf55bf27f7e92576f88015f213b
Aylin Görgün <aylingrgn2@gmail.com>
2020-02-26 16:23:31 +0300

    Week1 and Week2 labs.

---
+++ Week2-Terms/Lab2_Terms/Assets/Scripts/StartAwakeDifference.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAwakeDifference : MonoBehaviour
+{
+    void Awake(){
+        Debug.Log("awake is called");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start is called");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
