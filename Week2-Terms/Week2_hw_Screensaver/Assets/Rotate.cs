c53672c65a76e1e9898fafe6a78ee6f03b867fe7
Aylin Görgün <aylingrgn2@gmail.com>
2020-03-03 23:39:57 +0300

    Week2 Homework screensaver.

---
+++ Week2-Terms/Week2_hw_Screensaver/Assets/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public Vector3 rot;
+    public int speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rot * speed* Time.deltaTime);
+
+        if(Input.anyKey)
+            Application.Quit();
+    }
+}
