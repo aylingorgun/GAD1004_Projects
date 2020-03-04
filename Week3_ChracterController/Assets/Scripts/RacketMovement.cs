0119de443b3e7cce39d9258c824f8583739ba2f2
Aylin Görgün <aylingrgn2@gmail.com>
2020-03-04 11:12:43 +0300

    Week3_Pong

---
+++ Week3_ChracterController/Assets/Scripts/RacketMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketMovement : MonoBehaviour
+{
+
+    public float speed = 30;
+    public string axis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+  }
+}
---
+++ Week3_ChracterController/Assets/Scripts/spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject go;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(BallCollision.flag == true){
+            Invoke("Spawn", 2);
+            BallCollision.flag = false;
+        }
+        
+    }
+
+    void Spawn()
+    {
+        Instantiate(go);
+    }
+}
