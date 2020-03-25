38aa19be2ea00c2317dd9cf23deb03c795039de5
Aylin Görgün <aylingrgn2@gmail.com>
2020-03-25 14:40:46 +0300

    Week5 lab and homework.

---
+++ Week5_Physics/Week5_Lab_Physics/Assets/Scripts/CubeForce.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeForce : MonoBehaviour
+{
+    public float thrust = 200.0f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.up * thrust);
+    }
+}
---
+++ Week5_Physics/Week5_Lab_Physics/Assets/Scripts/RotateCube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    public float torque;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float turn = Input.GetAxis("Horizontal");
+        rb.AddTorque(transform.up * torque * turn);
+    }
+}
