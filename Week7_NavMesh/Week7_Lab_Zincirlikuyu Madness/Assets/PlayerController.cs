d39fbb922aa9bb9fcb6f48f562779a7dda9ee5ad
Aylin Görgün <aylingrgn2@gmail.com>
2020-04-30 03:54:52 +0300

    Week 8-9.

---
+++ Week7_NavMesh/Week7_Lab_Zincirlikuyu Madness/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject[] Targets;
+    public GameObject SelectedTarget;
+    public NavMeshAgent Agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectTarget();
+        Agent.SetDestination(SelectedTarget.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SelectTarget(){
+        SelectedTarget = Targets[Random.Range(0, 4)];
+    }
+
+}
---
+++ Week7_NavMesh/Week7_Lab_Zincirlikuyu Madness/Assets/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public int SpawnCount = 20;
+    public GameObject[] Chars;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharSpawner();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CharSpawner () {
+        for(int i=0; i < 20; i++)
+            Instantiate(Chars[Random.Range(0, 4)], this.gameObject.transform.position, Quaternion.identity);
+    }
+}
